b1d40a5bc8599f65857f842ebf011973013c2cf2
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-09-05 19:32:36 +0530

    check 2

--- src/components/UserTable.js
+++ src/components/UserTable.js
@@ -32,35 +32,45 @@ function UserTable() {
           Get User List
         </button>
       </div>
-      {showTable && ( // Conditionally render the table when showTable is true
-        <table className="user-table">
-          <thead>
-            <tr>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Avatar</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <tr key={user.id}>
-                <td>{user.first_name}</td>
-                <td>{user.last_name}</td>
-                <td>{user.email}</td>
-                <td>
-                  <img
-                    src={user.avatar}
-                    alt={`${user.first_name} ${user.last_name}`}
-                    width="50"
-                    height="50"
-                  />
-                </td>
+      <table className="user-table">
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Avatar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {showTable ? (
+            users.length > 0 ? (
+              users.map((user) => (
+                <tr key={user.id}>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    <img
+                      src={user.avatar}
+                      alt={`${user.first_name} ${user.last_name}`}
+                      width="50"
+                      height="50"
+                    />
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr className="no-data">
+                <td colSpan="5" >No data found to display.</td>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
+            )
+          ) : (
+            <tr className="no-data">
+              <td colSpan="5" >No data found to display.</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
